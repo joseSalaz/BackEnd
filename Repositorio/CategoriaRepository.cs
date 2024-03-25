9464d81537bc6fc0843c5bfe67c6152725996830
cesae32150 <i2221597@continental.edu.pe>
2024-03-25 12:14:33 -0500

    Cateogira

--- Repositorio/CategoriaRepository.cs
+++ Repositorio/CategoriaRepository.cs
@@ -1,6 +1,7 @@
 ﻿using DBModel.DB;
 using IRepositorio;
 using IRepository;
+using Microsoft.EntityFrameworkCore;
 using Repository.Generic;
 using System;
 using System.Collections.Generic;
@@ -15,6 +16,25 @@ namespace Repository
         public List<Categoria> GetAutoComplete(string query)
         {
             throw new NotImplementedException();
+        }
+        public CategoriaRepository() : base() { }
+
+        public async Task<List<Subcategoria>> GetSubcategoriasByCategoriaId(int categoriaId)
+        {
+            return await Task.Run(() =>
+            {
+                return dbSet.Where(c => c.Subcategoria.Any(sc => sc.IdCategoria == categoriaId))
+                    .SelectMany(c => c.Subcategoria)
+                    .ToList();
+            });
+        }
+
+        public async Task<List<Libro>> GetLibrosByCategoriaId(int categoriaId)
+        {
+            return await dbSet.Where(c => c.IdCategoria == categoriaId)
+                              .SelectMany(c => c.Subcategoria)
+                              .SelectMany(sc => sc.Libros)
+                              .ToListAsync();
         }
     }
 }
