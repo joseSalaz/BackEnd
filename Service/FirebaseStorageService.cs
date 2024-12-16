e8e587075c4d6b65a01ed13c7eb0864fa131a0b7
joseSalaz <josealvertosalazarchirinos@gmail.com>
2024-12-16 15:22:12 -0500

    cred

--- Service/FirebaseStorageService.cs
+++ Service/FirebaseStorageService.cs
@@ -15,19 +15,44 @@ namespace Service
     {
         private readonly FirebaseStorage _firebaseStorage;
 
+         public async Task<string> DownloadCredentialFileAsync(string blobUrl, string localPath)
+        {
+            using (var client = new HttpClient())
+            {
+                var response = await client.GetAsync(blobUrl);
+                if (response.IsSuccessStatusCode)
+                {
+                    var content = await response.Content.ReadAsByteArrayAsync();
+                    await File.WriteAllBytesAsync(localPath, content);
+                    return localPath; // Devuelve la ruta local donde guardaste el archivo
+                }
+                else
+                {
+                    throw new Exception("Failed to download credential file.");
+                }
+            }
+        }
         public FirebaseStorageService(IConfiguration configuration)
         {
+            // Definir la URL de las credenciales en la nube (Azure Blob Storage)
+            var blobUrl = configuration["Firebase:CredentialPath"];
+
+            // Ruta local temporal para guardar el archivo de credenciales descargado
+            var tempCredentialPath = Path.Combine(Path.GetTempPath(), "firebase-credentials.json");
+
+            // Descargar el archivo de credenciales
+            var localFilePath = DownloadCredentialFileAsync(blobUrl, tempCredentialPath).Result;
+
             if (FirebaseApp.DefaultInstance == null)
             {
                 FirebaseApp.Create(new AppOptions()
                 {
-                    Credential = GoogleCredential.FromFile(configuration["Firebase:CredentialPath"])
+                    Credential = GoogleCredential.FromFile(localFilePath) // Usa la ruta local
                 });
             }
 
             // Inicializa Firebase Storage
             var bucket = configuration["Firebase:StorageBucket"];
-
             _firebaseStorage = new FirebaseStorage(bucket, new FirebaseStorageOptions
             {
                 AuthTokenAsyncFactory = async () =>
@@ -37,6 +62,9 @@ namespace Service
                 }
             });
         }
+
+
+       
 
         public async Task<string> UploadFileAsync(IFormFile file, string folderName)
         {
