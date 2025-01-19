286d9999c16db7f163fa19c118b14fb4ab495eeb
cesae32150 <i2221597@continental.edu.pe>
2025-01-18 19:30:13 -0500

    ya esta

--- Service/AzureStorage.cs
+++ Service/AzureStorage.cs
@@ -1,44 +1,44 @@
-﻿using Azure.Storage.Blobs;
-using IService;
-using Microsoft.Extensions.Configuration;
-using System;
-using System.IO;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Http;
+﻿//using Azure.Storage.Blobs;
+//using IService;
+//using Microsoft.Extensions.Configuration;
+//using System;
+//using System.IO;
+//using System.Threading.Tasks;
+//using Microsoft.AspNetCore.Http;
 
-namespace Service
-{
-    public class AzureStorage : IAzureStorage
-    {
-        private readonly BlobServiceClient _blobServiceClient;
+//namespace Service
+//{
+//    public class AzureStorage : IAzureStorage
+//    {
+//        private readonly BlobServiceClient _blobServiceClient;
 
-        public AzureStorage(IConfiguration configuration)
-        {
-            string connectionString = Environment.GetEnvironmentVariable("AzureStorageConnectionString")
-                                      ?? configuration["AzureStorage:ConnectionString"];
+//        public AzureStorage(IConfiguration configuration)
+//        {
+//            string connectionString = Environment.GetEnvironmentVariable("AzureStorageConnectionString")
+//                                      ?? configuration["AzureStorage:ConnectionString"];
 
-            if (string.IsNullOrEmpty(connectionString))
-            {
-                throw new ArgumentNullException("AzureStorage:ConnectionString", "La cadena de conexión de Azure Storage no está configurada correctamente.");
-            }
+//            if (string.IsNullOrEmpty(connectionString))
+//            {
+//                throw new ArgumentNullException("AzureStorage:ConnectionString", "La cadena de conexión de Azure Storage no está configurada correctamente.");
+//            }
 
-            _blobServiceClient = new BlobServiceClient(connectionString);
-        }
+//            _blobServiceClient = new BlobServiceClient(connectionString);
+//        }
 
-        public async Task<string> SaveFile(string containerName, IFormFile file)
-        {
-            var blobContainerClient = _blobServiceClient.GetBlobContainerClient(containerName);
-            await blobContainerClient.CreateIfNotExistsAsync();
+//        public async Task<string> SaveFile(string containerName, IFormFile file)
+//        {
+//            var blobContainerClient = _blobServiceClient.GetBlobContainerClient(containerName);
+//            await blobContainerClient.CreateIfNotExistsAsync();
 
-            var fileName = $"{Guid.NewGuid()}{Path.GetExtension(file.FileName)}";
-            var blobClient = blobContainerClient.GetBlobClient(fileName);
+//            var fileName = $"{Guid.NewGuid()}{Path.GetExtension(file.FileName)}";
+//            var blobClient = blobContainerClient.GetBlobClient(fileName);
 
-            using (var stream = file.OpenReadStream())
-            {
-                await blobClient.UploadAsync(stream, true);
-            }
+//            using (var stream = file.OpenReadStream())
+//            {
+//                await blobClient.UploadAsync(stream, true);
+//            }
 
-            return blobClient.Uri.AbsoluteUri;
-        }
-    }
-}
+//            return blobClient.Uri.AbsoluteUri;
+//        }
+//    }
+//}
