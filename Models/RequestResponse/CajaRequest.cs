555b18274d1a020c34eaa698445367aef26d29ef
joseSalaz <josealvertosalazarchirinos@gmail.com>
2026-05-27 18:00:49 -0500

    Actualizado v 6 (2026)

--- Models/RequestResponse/CajaRequest.cs
+++ Models/RequestResponse/CajaRequest.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -20,8 +20,4 @@ namespace Models.RequestResponse
 
         public decimal? IngresosACaja { get; set; }
     }
-<<<<<<< HEAD
-  
-=======
->>>>>>> 62be587d6409086625d37c0110374bc6b627a483
 }
--- UtilMapper/AutoMapperProfiles.cs
+++ UtilMapper/AutoMapperProfiles.cs
@@ -1,4 +1,4 @@
-﻿using AutoMapper;
+using AutoMapper;
 using DBModel.DB;
 using Models.RequestRequest;
 using Models.RequestResponse;
@@ -101,16 +101,12 @@ namespace UtilMapper
             CreateMap<DireccionRequest, Direccion>();
             CreateMap<Direccion, DireccionRequest>();
             CreateMap<Direccion, DireccionResponse>();
-
-<<<<<<< HEAD
             CreateMap<FavoritoRequest, Favorito>().ReverseMap();
 
             CreateMap<Favorito, FavoritoResponse>()
                 .ForMember(dest => dest.TituloLibro, opt => opt.MapFrom(src => src.IdLibroNavigation.Titulo))
                 .ForMember(dest => dest.ImagenLibro, opt => opt.MapFrom(src => src.IdLibroNavigation.Imagen));
 
-=======
->>>>>>> 62be587d6409086625d37c0110374bc6b627a483
         }
 
     }
