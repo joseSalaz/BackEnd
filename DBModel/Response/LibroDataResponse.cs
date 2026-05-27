c494ad933ff32d1e2bf8e4748e13748322cd5a0f
joseSalaz <josealvertosalazarchirinos@gmail.com>
2026-05-27 17:50:43 -0500

    force

--- DBModel/Response/LibroDataResponse.cs
+++ DBModel/Response/LibroDataResponse.cs
@@ -10,32 +10,33 @@ namespace DBModel.Response
     {
         public int IdLibro { get; set; }
 
-        public string titulo { get; set; }
+        public string? titulo { get; set; }
 
-        public string ISBN { get; set; }
+        public string? ISBN { get; set; }
 
-        public string Tamanno { get; set; }
+        public string? Tamanno { get; set; }
 
-        public string Descripcion { get; set; }
+        public string? Descripcion { get; set; }
 
-        public string Condicion { get; set; }
+        public string? Condicion { get; set; }
 
-        public string Impresion { get; set; }
+        public string? Impresion { get; set; }
 
-        public string Tipo_Tapa { get; set; }
+        public string? Tipo_Tapa { get; set; }
 
-        public string estado { get; set; }
+        public string? estado { get; set; }
 
-        public string Categoria { get; set; }
+        public string? Categoria { get; set; }
 
-        public string Subcategoria { get; set; }
+        public string? Subcategoria { get; set; }
 
-        public string TipoPapel { get; set; }
+        public string? TipoPapel { get; set; }
 
-        public string Razon_Social { get; set; }
+        public string? Razon_Social { get; set; }
 
-        public string Imagen { get; set; }
+        public string? Imagen { get; set; }
 
-        public string autores { get; set; }
+        public string? autores { get; set; }
+        public decimal? PrecioVenta {get;set;}
     }
 }
